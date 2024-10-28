359df78d114cc0da220082380560d02252f027c7
Denis Arefyev <163993361+DenisArefyev@users.noreply.github.com>
2024-10-28 17:55:11 +0100

    Fixed errors

--- readme.txt
+++ readme.txt
@@ -4,8 +4,4 @@ npx create-next-app@latest
 npm run dev
 
 npm install mongodb
-global.d.ts
-declare global {
-  var _mongoClientPromise: Promise<MongoClient> | undefined;
-}
-export {};
+
--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,14 +1,12 @@
-// src/lib/mongodb.ts
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGO_DB_URI; // Убедитесь, что это строка
-let client: MongoClient | null = null; // Инициализация клиента как null
+const uri = process.env.MONGO_DB_URI;
+let client: MongoClient | null = null;
 
 if (!uri) {
   throw new Error('Please add your Mongo URI to .env');
 }
 
-// Функция для получения клиента MongoDB
 const getClient = async () => {
   if (!client) {
     client = new MongoClient(uri);
@@ -17,5 +15,4 @@ const getClient = async () => {
   return client;
 };
 
-// Экспортируем функцию для получения клиента
 export default getClient;
